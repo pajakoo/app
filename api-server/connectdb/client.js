edb4e6cb73590a18c8defac5a48923be9a92a6cc
krasimir.georgiev <krasimir.georgiev@novarto.com>
2024-01-23 18:38:28 +0200

    db

--- api-server/connectdb/client.js
+++ api-server/connectdb/client.js
@@ -1,7 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_CONNECTION}@cluster0.udwqatw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-//const uri = "mongodb://localhost:27017/"
-
+const uri = process.env.DB_CLIENT_URL;
+console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
