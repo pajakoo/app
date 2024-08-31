9125b13876363074f0a5df07a03d86c4de3137cb
krasimir.georgiev <krasimir.georgiev@novarto.com>
2024-08-31 14:12:01 +0300

    fix focus

--- price-hunter/src/hooks/useAutoBlur.js
+++ price-hunter/src/hooks/useAutoBlur.js
@@ -25,8 +25,27 @@ const useAutoBlur = () => {
       }
     };
 
+    const handleInputClick = (event) => {
+      if( event.target.tagName === 'A' ) {
+        const linkElement = event.target;
+        if (linkElement) {
+          // Find the closest input field relative to the 'A' element.
+          const closestInput = linkElement.closest('form')?.querySelector('input, textarea, select');
+      
+          if (closestInput) {
+            // Perform any actions you need on the closest input
+            console.log('Closest input field:', closestInput);
+            // Example: focus on the input field
+            closestInput.blur();
+          } else {
+            console.log('No input field found near the clicked link');
+          }
+        }
+      }
+    };
+
     document.addEventListener('change', handleInputChange);
-    document.addEventListener('click', handleInputChange);
+    document.addEventListener('click', handleInputClick);
 
     return () => {
       document.removeEventListener('change', handleInputChange);
